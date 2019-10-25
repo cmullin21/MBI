ecfdbd2c450c6e5b3aff8ca23d202a3c9aeae4ae
SuzannaCarter <suzannacarter@gmail.com>
2019-10-24 20:18:42 -0400

    Remove "Ledger" button from the header

--- application/views/headers/accountantHeader.php
+++ application/views/headers/accountantHeader.php
@@ -39,9 +39,6 @@
                   <a class="dropdown-item" href="<?php echo site_url('accountant/AccountantJournalController/');?>">View Journals</a>
                 </div>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Ledger</a>
-              </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Reports
