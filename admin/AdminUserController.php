46d356baaec59fb76f1285946618d785767a3cad
Aaron Badgett <32856818+abadget1@users.noreply.github.com>
2019-10-26 23:48:39 -0400

    Validation Changes
    Registration page
    Add/Edit Accounts 
    Add/Edit Users

---
+++ admin/AdminUserController.php
@@ -0,0 +1,93 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdminUserController extends CI_Controller {
+
+  public function __construct(){
+    parent::__construct();
+    $this->load->model('User_model');
+  }
+
+  public function index(){
+    $data['result'] = $this->User_model->getAllData();
+    $this->load->view('headers/adminHeader');
+    $this->load->view('admin/adminUser', $data);
+    $this->load->view('footers/footer');
+  }
+
+  public function create(){
+    $this->User_model->createData();
+    redirect("admin/AdminUserController");
+  }
+
+  // ----- New -----
+  public function createNewUser(){
+
+    // username availability is checcked after submission
+    $this->form_validation->set_rules('username', 'Username', 'trim|is_unique[users.username]');
+    $this->form_validation->set_rules('email', 'Email', 'trim|is_unique[users.email]');
+
+    if ($this->form_validation->run() == TRUE) {
+
+       $data = array(
+           //assign user data into array elements
+           'firstname' => $this->input->post('firstname'),
+           'lastname' => $this->input->post('lastname'),
+           'birthday' => $this->input->post('birthday'),
+           'email' => $this->input->post('email'),
+           'address' => $this->input->post('address'),
+           'level' => $this->input->post('level'),
+           'username' => $this->input->post('username'),
+           'active' => $this->input->post('active'),
+           'password' => sha1($this->input->post('password'))
+         );
+       $this->User_model->addUser($data);
+       //set message to be shown when registration is completed
+       $this->session->set_flashdata('success','User is registered!');
+       redirect('admin/AdminUserController');
+    } else {
+           //return to the same page again with validation errors
+           redirect($this->uri->uri_string());
+       }
+  }
+  // ----- New -----
+  public function updateUser(){
+    $this->form_validation->set_rules('username', 'Username', 'trim|is_unique[users.username]');
+    if ($this->form_validation->run() == TRUE) {
+
+       $data = array(
+           //assign user data into array elements
+           'firstname' => $this->input->post('firstname'),
+           'lastname' => $this->input->post('lastname'),
+           'birthday' => $this->input->post('birthday'),
+           'email' => $this->input->post('email'),
+           'address' => $this->input->post('address'),
+           'level' => $this->input->post('level'),
+           'username' => $this->input->post('username'),
+           'active' => $this->input->post('active'),
+           'password' => sha1($this->input->post('password'))
+         );
+       $this->User_model->updateUser($data);
+       //set message to be shown when registration is completed
+       $this->session->set_flashdata('success','User updated!');
+       redirect('admin/AdminUserController');
+    } else {
+           //return to the same page again with validation errors
+           redirect($this->uri->uri_string());
+       }
+  }
+
+  public function edit($ID){
+    $data['row'] = $this->User_model-> getData($ID);
+    $this->load->view('headers/adminHeader');
+    $this->load->view('admin/adminUserEdit', $data);
+    $this->load->view('footers/footer');
+  }
+
+  public function update($ID) {
+    $this->User_model->updateData($ID);
+    redirect("admin/AdminUserController");
+  }
+
+
+}
---
+++ models/COA_model.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class COA_model extends CI_Model {
+
+  public function __construct(){
+    $this->load->database();
+  }
+
+  function createData(){
+    $data = array(
+      'accountName' => $this->input->post('accountName'),
+      'accountNumber' => $this->input->post('accountNumber'),
+      'accountDescription' => $this->input->post('accountDescription'),
+      'normalSide' => $this->input->post('normalSide'),
+      'accountCategory' => $this->input->post('accountCategory'),
+      'accountSubcategory' => $this->input->post('accountSubcategory'),
+      'initialBalance' => $this->input->post('initialBalance'),
+      'debit' => $this->input->post('debit'),
+      'credit' => $this->input->post('credit'),
+      'balance' => $this->input->post('balance'),
+      'addedBy' => $this->input->post('addedBy'),
+      'accountOrder' => $this->input->post('accountOrder'),
+      'statement' => $this->input->post('statement'),
+      'comment' => $this->input->post('comment')
+    );
+    $this->db->insert('chartsofaccounts', $data);
+  }
+
+  function addAccount($data){
+    // insert data into database
+    $this->db->insert('chartsofaccounts', $data);
+  }
+
+  function getAllData($providedQuery){
+    if ($providedQuery != "normal"){
+      $query = $this->db->query($providedQuery);
+      return $query->result();
+    } else {
+      $query = $this->db->query('SELECT * FROM chartsofaccounts');
+      return $query->result();
+    }
+  }
+
+  function getSubcategories(){
+    $query = $this->db->query('SELECT subcategoryName FROM subcategories');
+    return $query->result();
+  }
+
+  function getData($accountNumber) {
+    $query = $this->db->query('SELECT * FROM chartsofaccounts WHERE `accountNumber` =' .$accountNumber);
+    return $query->row();
+  }
+  // ----- NEW -----
+  function updateAccount($data){
+    // edit user based on row/ID
+    // ... Coming soon
+  }
+  function updateData($accountNumber) {
+    $data = array(
+      'accountName' => $this->input->post('accountName'),
+      'accountNumber' => $this->input->post('accountNumber'),
+      'accountDescription' => $this->input->post('accountDescription'),
+      'normalSide' => $this->input->post('normalSide'),
+      'accountCategory' => $this->input->post('accountCategory'),
+      'accountSubcategory' => $this->input->post('accountSubcategory'),
+      'statement' => $this->input->post('statement'),
+      'comment' => $this->input->post('comment')
+    );
+    $this->db->where('accountNumber', $accountNumber);
+    $this->db->update('chartsofaccounts', $data);
+  }
+
+}
