10f47790fffd9925da59741db6197aa4f2ebf8e3
Cody <cmullin21@gmail.com>
2019-10-28 19:30:01 -0400

    Updated Login to check if user is active or not

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -19,21 +19,35 @@ class Login extends CI_Controller {
     if($result->num_rows() > 0){
         $data = $result->row_array();
         $name = $data['username'];
+        $active = $data['active'];
         $email = $data['email'];
         $level = $data['level'];
         $sesdata = array(
             'username' => $username,
             'email' => $email,
             'level' => $level,
+            'active' => $active,
             'logged_in' => TRUE
         );
         $this->session->set_userdata($sesdata);
         if($level === 'Admin'){
+          if($active === 'Yes'){
             redirect('admin/Admin');
+          } else{
+            echo "<script>alert('User not active'); history.go(-1);</script>";
+          }
         } elseif($level === 'Manager') {
+          if($active === 'Yes'){
             redirect('manager/Manager');
+          } else{
+            echo "<script>alert('User not active'); history.go(-1);</script>";
+          }
         } else{
+          if($active === 'Yes'){
             redirect('accountant/Accountant');
+          } else{
+            echo "<script>alert('User not active'); history.go(-1);</script>";
+          }
         }
     } else{
         echo "<script>alert('access denied'); history.go(-1);</script>";
