b511791e4ccc84a33a52f0c5b7cd04fe7f34c6cb
Joseph Nguyen <40273733+josephn268@users.noreply.github.com>
2019-10-28 21:47:03 -0400

    JQuery code for filtering data from table

--- application/views/footers/footer.php
+++ application/views/footers/footer.php
@@ -1,2 +1,21 @@
   </body>
+<script type="text/javascript">
+  $(document).ready(function() {
+
+// key up from an input with class filter
+$('.filter').on('keyup', function() {
+
+     // create regular expression from value of the input
+     var rex = new RegExp($(this).val(), 'i');
+
+     // hide all <tr>
+     $('.searchable tr').hide();
+
+     // show all <tr> which contains your text
+     $('.searchable tr').filter(function() {
+          return rex.test($(this).text());
+     }).show();
+ }); 
+}); 
+</script>
 </html>
