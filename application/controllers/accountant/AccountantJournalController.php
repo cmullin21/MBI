414988c183c8cd78aa9b56a689f3a4bc2dd48f2c
Joseph Nguyen <40273733+josephn268@users.noreply.github.com>
2019-11-08 01:27:25 -0500

    Updated createData() for file upload

--- application/controllers/accountant/AccountantJournalController.php
+++ application/controllers/accountant/AccountantJournalController.php
@@ -19,6 +19,19 @@ class AccountantJournalController extends CI_Controller {
 
   public function createData(){
     $result = $this->Journal_model->batchInsert($_POST);
+    /*
+    // Upload the files then pass data to your model
+    $config['upload_path'] = '.uploads/documents';
+    $config['allowed_types'] = 'pdf|docx|txt';
+    $this->load->library('upload', $config);
+    if (!$this->upload->do_upload('userfile')){
+      // If the upload fails
+      echo $this->upload->display_errors('<p>', '</p>');
+    }else{
+      // Pass the full path and post data 
+      $this->Journal_model->batchInsert($this->upload->data('full_path'),$this->input->post());
+      $this->load->view('accountant/accountantJournal');
+    }*/
     $this->load->view('headers/accountantHeader');
     $this->load->view('accountant/accountantJournal');
     $this->load->view('footers/footer');
